fe4dd573cba41361b4c64d9abf6cca199e6e21dd
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2023-09-17 12:54:11 +0700

    go-challenge

--- configs/mysql.go
+++ configs/mysql.go
@@ -4,12 +4,25 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 )
 
 func NewMySQL() (*sql.DB, error) {
 	user := os.Getenv("MYSQL_USER")
 	password := os.Getenv("MYSQL_PASSWORD")
 	dbName := os.Getenv("MYSQL_DATABASE")
+	dbPort := os.Getenv("MYSQL_PORT")
+	dbHost := os.Getenv("MYSQL_HOST")
 
-	return sql.Open("mysql", fmt.Sprintf("%s:%s@/%s?parseTime=true", user, password, dbName))
+	sql, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, dbHost, dbPort, dbName))
+
+	if err != nil {
+		return nil, err
+	}
+
+	sql.SetConnMaxIdleTime(15 * time.Minute)
+	sql.SetMaxIdleConns(10)
+	sql.SetMaxOpenConns(50)
+
+	return sql, nil
 }
